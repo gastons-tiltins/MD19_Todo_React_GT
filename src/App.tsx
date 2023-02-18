09ef70375ab3173789db32e8dc510e4064f44e8f
Gastons Tiltins <gastons.tiltins@gmail.com>
2023-02-18 04:00:32 +0200

    frontend/backend working, CONTEXT not done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import reset from './assets/reset.css';
 import reactLogo from './assets/react.svg';
 import {nanoid} from 'nanoid';
@@ -12,60 +12,86 @@ type Task = {
 };
 
 function App() {
-    const [tasks, setTasks] = useState([]);
+    const [value, setValue] = useState('');
 
-    const [formValue, setValue] = useState('');
-
-    const handleDone = (id: string) => {
-        // task.map((t, index) => {
-        //     if (t.id === id) {
-        //         task[index].status = 1;
-        //         setTask([...task]);
-        //     }
-        // });
-    };
-
-    // Work with MongoDB/API
+    const onInput = (e: any) => setValue(e.target.value);
 
     // Access the client
     const queryClient = useQueryClient();
 
-    // Send data to API
-    const sendDataToApi = useMutation({
-        mutationFn: (taskData) => {
-            return axios.post(`http://localhost:3004/post`, taskData);
+    const getTasks = async () => {
+        const res = await axios.get(`http://localhost:3004/tasks`);
+        return res.data;
+    };
+
+    // Queries
+    const {isLoading, error, data} = useQuery(['taskList'], getTasks);
+
+    // Modify the task
+    const addTask = useMutation({
+        mutationFn: (newTask) => {
+            return axios.post(`http://localhost:3004/post`, newTask);
         },
         onSuccess: () => {
-            // Invalidate and refetch
-            queryClient.invalidateQueries({queryKey: ['repoData']});
+            queryClient.invalidateQueries({queryKey: ['taskList']});
         },
     });
 
-    const deleteApiTask = useMutation({
+    // Modify the task
+    const modTask = useMutation({
+        mutationFn: (modTaskData) => {
+            return axios.patch(`http://localhost:3004/tasks`, modTaskData);
+        },
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries({queryKey: ['taskList']});
+        },
+    });
+
+    // Delete the comment
+    const deleteTask = useMutation({
         mutationFn: (taskId) => {
             return axios.delete(`http://localhost:3004/delete/${taskId}`);
         },
         onSuccess: () => {
             // Invalidate and refetch
-            queryClient.invalidateQueries({queryKey: ['repoData']});
+            queryClient.invalidateQueries({queryKey: ['taskList']});
         },
     });
+
+    const handleDone = (taskId: any) => {
+        axios
+            .post('http://localhost:3004/update', {
+                id: taskId,
+                status: 1,
+            })
+            .then((response) => {
+                queryClient.invalidateQueries({queryKey: ['taskList']});
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     const handleDelete = (taskId: any) => {
         // const deleteTask = task.filter((_, i) => i !== _id);
         console.log(taskId);
-        deleteApiTask.mutate(taskId);
+        deleteTask.mutate(taskId);
     };
 
     const handleAddTask = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const newTask: any = {
-            id: nanoid(),
-            task: formValue,
+            task: value,
             status: 0,
         };
-        sendDataToApi.mutate(newTask);
+        addTask.mutate(newTask);
+        setValue('');
     };
+
+    if (isLoading) return <h1>Loading...</h1>;
+
+    if (error) return <h1>An error has occurred.</h1>;
 
     const Tasklist = ({data}: any) => {
         return (
@@ -97,7 +123,9 @@ function App() {
                                 <div>{task.task}</div>
                                 {/* <div>{task.status}</div> */}
                                 <div className='gridStyleDone'>
-                                    <button onClick={() => handleDone(task.id)}>
+                                    <button
+                                        onClick={() => handleDone(task._id)}
+                                    >
                                         Done
                                     </button>
                                 </div>
@@ -117,45 +145,18 @@ function App() {
         );
     };
 
-    // Fetch data from API
-    const FetchSqlData = () => {
-        const {isLoading, error, data} = useQuery<any>({
-            queryKey: ['repoData'],
-            queryFn: () =>
-                fetch('http://localhost:3004/tasks').then((res) => res.json()),
-            onSuccess: () => {
-                // Invalidate and refetch
-                queryClient.invalidateQueries({queryKey: ['repoData']});
-            },
-        });
-
-        if (isLoading) return <div>'Loading...'</div>;
-
-        if (error) return <>'An error has occurred: ' + error.message;</>;
-
-        const taskInputChange = (e: any) => {
-            e.preventDefault;
-            setValue(e.target.value);
-        };
-
-        return (
-            <div className='App'>
-                <h1>Todo list</h1>
-                <>
-                    <form onSubmit={handleAddTask}>
-                        <input
-                            type='text'
-                            // value={formValue}
-                            onChange={taskInputChange}
-                        />
-                        <button>Add Task</button>
-                    </form>
-                </>
-                <Tasklist data={data} />
-            </div>
-        );
-    };
-    return <FetchSqlData />;
+    return (
+        <div className='App'>
+            <h1>Todo list</h1>
+            <>
+                <form onSubmit={handleAddTask}>
+                    <input type='text' onChange={onInput} value={value} />
+                    <button>Add Task</button>
+                </form>
+            </>
+            <Tasklist data={data} />
+        </div>
+    );
 }
 
 export default App;
